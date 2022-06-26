381b102c9b134f3a9ec87852c73d312ac7f7431b
faustaciku <faustaciku@gmail.com>
2022-06-26 17:30:37 +0100

    Stored all the comment's values and displayed it in a paragraph

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ form.addEventListener("submit", displayComment);
 
 function displayComment(event) {
   event.preventDefault();
+  let nameInput = document.querySelector("#name");
+  let emailInput = document.querySelector("#email");
   let commentInput = document.querySelector("#comment");
   console.log(commentInput.value);
+  console.log(nameInput.value);
+  console.log(emailInput.value);
+  let commentBox = document.querySelector("p");
+  commentBox.innerHTML =
+    nameInput.value + emailInput.value + commentInput.value;
 }
